d3c69fd2bbf17f21721ff190d772ffabf5aef2df
Nicolas Oliveira Mor <nicolasoliveiramor05@gmail.com>
2025-01-03 02:18:44 -0300

    commit inicial

---
+++ src/containers/Produtos/index.tsx
@@ -0,0 +1,24 @@
+import Produto from '../../components/Produto'
+import { useGetJogosQuery } from '../../services/api'
+
+import * as S from './styles'
+
+const Produtos = () => {
+  const { data: jogos, isLoading, error } = useGetJogosQuery()
+
+  if (isLoading) return <h2>Carregando...</h2>
+
+  if (error) return <h2>Erro ao carregar os produtos</h2>
+
+  return (
+    <>
+      <S.Produtos>
+        {jogos?.map((game) => (
+          <Produto key={game.id} game={game} />
+        ))}
+      </S.Produtos>
+    </>
+  )
+}
+
+export default Produtos
